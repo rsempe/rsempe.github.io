b8cc6192d06e8ef503071bd164e65bb785b529ad
Romain Sempé <romain.sempe@qonto.eu>
2022-07-13 00:01:13 +0200

    Add action mailer configuration

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,9 @@
+# -*- encoding : utf-8 -*-
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: "giteslescelestins@gmail.com",
+  password: "nbhuyxgsybkkodgx",
+  authentication: "plain"
+}
