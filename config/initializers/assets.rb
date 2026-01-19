6877f3156ff50328701bfe56065c45942d17a8d1
Romain Sempé <romain@getlago.com>
2026-01-19 22:00:52 +0100

    Fix JS assets loading in production
    - Remove hardcoded /assets/ paths from javascript_include_tag
    - Add app/javascript to assets paths
    - Add JS files to precompile list

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,9 +4,16 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("app/javascript")
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[
+  jquery-1.11.2.min.js
+  common_scripts_min.js
+  validate.js
+  functions.js
+  greensock.js
+  layerslider.transitions.js
+  layerslider.kreaturamedia.jquery.js
+  vegas.js
+]
